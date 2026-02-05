532058a2ad547118158c98884b3437ee34dcae43
shiva <shiva@192.168.1.38>
2026-02-05 22:50:50 +0530

    Property file read

--- SeleniumActitime_Copilot/src/test/java/ActitimeLoginPackage/Login.java
+++ SeleniumActitime_Copilot/src/test/java/ActitimeLoginPackage/Login.java
@@ -7,36 +7,36 @@ import org.testng.annotations.Test;
 
 public class Login {
 
-	@Test(priority = 1)
+    @Test(priority = 1)
+    public void loginToActitime() {
+        WebDriver driver = new ChromeDriver();
+        String url = ExeclReadData.getProperty("url");
+        
+        driver.get(url);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(30));
+        // Enter username
+        driver.findElement(By.name("username")).sendKeys(ExeclReadData.getProperty("username"));
+        // Enter password
+        driver.findElement(By.name("pwd")).sendKeys(ExeclReadData.getProperty("password"));
+        // Click on login button
+        driver.findElement(By.id("loginButton")).click();
+        // Optionally, add a wait or assertion here to verify login success
+    }
 
-	public void loginToActitime() {
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://127.0.0.1/login.do;jsessionid=d4ddniitgcaoj");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(30));
-		// Enter username
-		driver.findElement(By.name("username")).sendKeys("admin");
-		// Enter password
-		driver.findElement(By.name("pwd")).sendKeys("manager");
-		// Click on login button
-		driver.findElement(By.id("loginButton")).click();
-
-		// Optionally, add a wait or assertion here to verify login success
-	}
-
-	// add two more test methods for invalid login and logout functionality
-	@Test(priority = 2)
-	public void invalidLoginToActitime() {
-		WebDriver driver = new ChromeDriver();
-		driver.get("http://127.0.0.1/login.do;jsessionid=d4ddniitgcaoj");
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(30));
-		// Enter invalid username
-		driver.findElement(By.name("username")).sendKeys("invalid");
-		// Enter invalid password
-		driver.findElement(By.name("pwd")).sendKeys("invalid");
-		// Click on login button
-		driver.findElement(By.id("loginButton")).click();
-
-	}
+    // add two more test methods for invalid login and logout functionality
+    @Test(priority = 2)
+    public void invalidLoginToActitime() {
+        WebDriver driver = new ChromeDriver();
+        String url = ExeclReadData.getProperty("url");
+        driver.get(url);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(30));
+        // Enter invalid username
+        driver.findElement(By.name("username")).sendKeys("invalid");
+        // Enter invalid password
+        driver.findElement(By.name("pwd")).sendKeys("invalid");
+        // Click on login button
+        driver.findElement(By.id("loginButton")).click();
+    }
 }
